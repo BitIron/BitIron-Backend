cd96c68b8a203803574ffd74abe76bdfa4adfc5c
Marcos203403 <a29004@svalero.com>
2026-04-24 22:52:42 +0200

    feat: implement CRUD operations and routes for products module

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./config/db');
 const categoriaRoutes = require('./routes/categoriaRoutes');
+const productoRoutes = require('./routes/productoRoutes');
 
 
 
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/categorias', categoriaRoutes);
+app.use('/api/productos', productoRoutes);
 
 const PORT = process.env.PORT || 3000;
 
