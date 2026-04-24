e5b2ff8da263129a21fcf67f9dd60686da49ecc0
Marcos203403 <a29004@svalero.com>
2026-04-24 22:40:36 +0200

    fix: añadir rutas de categorias al index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
 const pool = require('./config/db');
+const categoriaRoutes = require('./routes/categoriaRoutes');
+
+
+
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/categorias', categoriaRoutes);
 
 const PORT = process.env.PORT || 3000;
 
