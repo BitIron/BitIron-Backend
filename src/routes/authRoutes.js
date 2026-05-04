4db5834162171cd4d6829b91652972b6aba62828
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-05-04 17:28:28 +0200

    feat: implement authentication system with JWT, request validation, and add Postman collection

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const rateLimit = require('express-rate-limit');
+const { registroValidator, loginValidator } = require('../validators/authValidator'); // Importamos los validadores
 
 const authController = require('../controllers/authController');
 
 const loginLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutos
-    max: 5, // Limita cada IP a 5 peticiones por 'window' (por 15 minutos)
+    max: 5, // Limita cada IP a 5 peticiones
     message: { error: 'Demasiados intentos de inicio de sesión desde esta IP. Por favor, inténtalo de nuevo en 15 minutos.' },
     standardHeaders: true,
     legacyHeaders: false,
 });
 
-// Rutas de autenticación
-router.post('/registro', authController.registro);
-router.post('/login', loginLimiter, authController.login);
+// Rutas de autenticación con validaciones inyectadas
+router.post('/registro', registroValidator, authController.registro);
+
+router.post('/login', loginLimiter, loginValidator, authController.login);
 
 module.exports = router;
