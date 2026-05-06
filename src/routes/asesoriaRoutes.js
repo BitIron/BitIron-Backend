e08de6115a40fda57a62663347210928a9f16201
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-05-06 20:09:35 +0200

    feat: add robust validation for consultation management

--- src/routes/asesoriaRoutes.js
+++ src/routes/asesoriaRoutes.js
@@ -4,10 +4,12 @@ const asesoriaController = require('../controllers/asesoriaController');
 // Podríamos añadir verificarToken o esAdmin aquí si quisiéramos protegerlas
 // const { verificarToken, esAdmin } = require('../middlewares/authMiddleware');
 
+const { asesoriaCreateValidator, asesoriaUpdateValidator } = require('../validators/asesoriaValidator');
+
 router.get('/', asesoriaController.getAll);
 router.get('/:id', asesoriaController.getById);
-router.post('/', asesoriaController.create);
-router.put('/:id', asesoriaController.update);
+router.post('/', asesoriaCreateValidator, asesoriaController.create);
+router.put('/:id', asesoriaUpdateValidator, asesoriaController.update);
 router.delete('/:id', asesoriaController.remove);
 
 module.exports = router;
