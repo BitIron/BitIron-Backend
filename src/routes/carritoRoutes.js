2aa659808530afef43344a8fd3b1e613450fc8a3
Marcos203403 <a29004@svalero.com>
2026-05-21 15:36:03 +0200

    fix(security): protect checkout, order history and asesoria routes with JWT; fix DELETE /carrito/clear route conflict

--- src/routes/carritoRoutes.js
+++ src/routes/carritoRoutes.js
@@ -7,7 +7,7 @@ const { verificarToken } = require('../middlewares/authMiddleware');
 router.get('/', verificarToken, carritoController.getCarrito);
 router.post('/', verificarToken, validateCarrito, carritoController.agregarAlCarrito);
 router.put('/:id', verificarToken, validateUpdateCantidad, carritoController.actualizarCantidad);
-router.delete('/:id', verificarToken, carritoController.eliminarDelCarrito);
 router.delete('/clear', verificarToken, carritoController.vaciarCarrito);
+router.delete('/:id', verificarToken, carritoController.eliminarDelCarrito);
 
 module.exports = router;
